6087021710a60929796f90c84bf08d9f9e649a08
Christopher Czach <chrisczach@gmail.com>
2018-12-16 01:18:13 -0800

    nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -12,7 +12,12 @@ const Nav = props => {
           <div  className={props.navOpen ? classes.closeMenu : classes.openMenu} />
       </div>
       <nav className={props.navOpen ? classes.dropDown : classes.dropDown + ' ' + classes.hide}>
-      Nav
+        <div className={classes.navWrapper}>
+          <div className={classes.navLink}>Link One</div>
+          <div className={classes.navLink}>Link Two</div>
+          <div className={classes.navLink}>Link Three</div>
+          <div className={classes.navLink}>Link Four</div>
+      </div>
       </nav>
       </>
   )
