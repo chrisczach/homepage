b58b73920c84214add1a5ea19c23d245e24ba554
Christopher Czach <chrisczach@gmail.com>
2018-12-17 19:25:55 -0800

    popup

--- src/components/projects/popup/Popup.js
+++ src/components/projects/popup/Popup.js
@@ -4,11 +4,15 @@ import classes from './Popup.module.css';
 const Popup = props => {
 
   return (
+    <>
+      <div onClick={props.closeHandler} className={props.shown ? classes.background : classes.background + ' ' + classes.hideBackground} />
     <div className={props.shown ? classes.wrap : classes.wrap + ' ' + classes.hide}>
       <header className={classes.title}>{props.popupObject.title}</header>
       <div className={classes.close} onClick={props.closeHandler}>X</div>
       <iframe className={classes.frame} src={props.popupObject.url} />
-    </div>
+      </div>
+
+      </>
   )
 }
 
