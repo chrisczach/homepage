d1e83f33cd4a8714d0cdde9547add970fdc9610b
Christopher Czach <chrisczach@gmail.com>
2018-12-18 00:14:24 -0800

    form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,14 +5,22 @@ import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor';
 const Contact = props => {
 
   return(
-    <section className={classes.contact}>
+    <form className={classes.contact} name="contact" method="POST" data-netlify="true">
       <ScrollableAnchor id={'contact'}>
         <header className={classes.heading}>contact</header>
       </ScrollableAnchor>
-      <div className={classes.contactForm}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-     </div>
-    </section>
+    <label className={classes.labels} for="name">Name </label>   <input className={classes.inputs} placeholder="Full Name" type="text" name="name" required/>
+
+    <label className={classes.labels} for="email">Email </label><input className={classes.inputs} placeholder="Email Address" type="email" name="email" required/>
+
+    <label className={classes.labels} for="subect">Subject </label><textarea  className={classes.inputs} placeholder="Subject" name="subject"></textarea>
+ 
+    <label className={classes.labels} for="message">Message </label><textarea  className={classes.inputs}  placeholder="Message"  name="message" required></textarea>
+ 
+      <button className={classes.submit}type="submit">Send</button>
+
+</form>
+ 
   )
 }
 
